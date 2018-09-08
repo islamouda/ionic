6c53fd39e24b41177c0eadef3ce8e59ca4ae1bce
islam.jameel9@gmail.com <islam.jameel9@gmail.com>
2018-09-08 10:50:39 +0300

    ionic 3 22 to 30 tut

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,8 +3,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service'
 import { TabsPage } from '../tabs/tabs';
 import { RegisterPage } from '../register/register';
-
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase';
+import { ContactPage } from '../contact/contact'
 
 /**
  * Generated class for the LoginPage page.
@@ -31,7 +32,8 @@ export class LoginPage {
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    public authServiceProvider: AuthServiceProvider) {
+    public authServiceProvider: AuthServiceProvider,
+  public fire:AngularFireAuth) {
   }
 
   ionViewDidLoad() {
@@ -57,6 +59,26 @@ this.authServiceProvider.signInWithEmail(credentials).then(
 
 goToRegister(){
   this.navCtrl.push(RegisterPage)
+}
+
+
+logINFacebook(){
+  this.fire.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(res => {
+    this.navCtrl.setRoot(ContactPage),
+    console.log(res);
+    
+  })
+
+}
+
+
+logINGoogle(){
+  this.fire.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(res => {
+    this.navCtrl.setRoot(ContactPage),
+    console.log(res);
+    
+  })
+
 }
 
 
